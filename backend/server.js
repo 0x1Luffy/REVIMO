c7eb309fe46530e996cd58efc78e1a9a046573fd
0x1Luffy <chetankesare06@gmail.com>
2024-12-24 20:46:22 +0530

    WIP: Fixed Diployment Error.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const dbConnection = require("./config/db");
+import axios from "axios";
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const fs = require("fs");
