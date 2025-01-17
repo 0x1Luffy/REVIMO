c4700bea8ddd994f2affc01c6019b55e181ee4dd
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-17 11:12:41 +0530

    Updated vite.config.js to be compatible with Docker.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Allows access from external networks (e.g., Docker, remote servers)
+    port: 5173,  // Optional: Set the port to 5173 (or any port you prefer)
+  },
 })
