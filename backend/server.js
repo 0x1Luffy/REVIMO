1f5c3bf6c464cf42561ef2d6aa8fa310b0220101
Chetan Kesare <124528370+0x1Luffy@users.noreply.github.com>
2025-01-17 10:41:42 +0530

    Changed Backend Port from 8080 to 4000
    Changed the port because it was overlapping with Jenkins Server.

--- backend/server.js
+++ backend/server.js
@@ -72,6 +72,6 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
